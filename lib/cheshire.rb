6d589f2b8cc218c78502f8d76d60e361552bb65d
speric <eric@prudentiadigital.com>
2013-03-12 19:11:58 -0400

    Working version.

--- lib/cheshire.rb
+++ lib/cheshire.rb
@@ -1,19 +1,23 @@
 module Cheshire
   def method_missing(meth, *args, &block)
-    fields = meth.to_s.split("_and_")
-    if fields.split("_and_").length > 0 && fields.each { |field| respond_to?(field.to_sym) }
-      run_conjoin(fields)
+    delegate_to(meth) { |obj_properties| run_conjoin(obj_properties)}
+  end
+  
+  def delegate_to(meth)
+    properties = meth.to_s.split("_and_")
+    if properties.split("_and_").length > 0 && properties.each { |property| respond_to?(property.to_sym) }
+      yield properties
     else
       super
     end
   end
-  
-  def respond_to?()
-  end
-  
+
   def run_conjoin(fields)
     foo = String.new
     fields.each {|field| foo << "#{public_send(field.to_sym)} "}
-    foo.chomp
+    foo.rstrip
   end
+
+  #implement respond_to?
+  #chain like a.public_send(:shoots).public_send(:first).public_send(:name)
 end
