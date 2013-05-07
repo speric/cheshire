ef1c238a7526c4543c7117cbda45129d4392cbcc
Eric Farkas <eric@prudentiadigital.com>
2013-05-07 16:14:32 -0300

    Update cheshire.rb

--- lib/cheshire.rb
+++ lib/cheshire.rb
@@ -2,13 +2,14 @@ module Cheshire
   def method_missing(meth, *args, &block)
     properties = meth.to_s.split("_and_")
     if properties.length > 0 && properties.each { |property| respond_to?(property.to_sym) }
-      contatenate properties
+      concatenate properties
     else
       super
     end
   end
   
-  def contatenate(fields)
+  def concatenate(fields)
+    #or build an array of fields and use join(' ')
     str = String.new
     fields.each {|field| str << "#{public_send(field.to_sym)} "}
     str.rstrip!
